4fe0f445c98784b5c25a5e42aa65eb2afccb1b71
Shybert <25749775+Shybert@users.noreply.github.com>
2020-06-09 13:32:18 +0200

    Simple inline JS minification pipeline with Terser

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,20 @@
 const MarkdownIt = require("markdown-it");
+const Terser = require("terser");
 
 module.exports = function (eleventyConfig) {
   const md = new MarkdownIt();
   eleventyConfig.addPairedShortcode("markdown", (content) =>
     md.render(content.trimStart())
   );
+
+  eleventyConfig.addFilter("jsmin", (code) => {
+    const minifiedCode = Terser.minify(code, { ecma: 5 });
+    if (minifiedCode.error) {
+      console.error("JS minification error: ", minifiedCode.error);
+      return code;
+    }
+    return minifiedCode.code;
+  });
 
   eleventyConfig.setTemplateFormats(["njk", "css", "svg"]);
 
