69b418b49924716ec090afc340b11bdede662182
Shybert <25749775+Shybert@users.noreply.github.com>
2020-09-09 12:17:58 +0200

    PDF version of the program now actually displays the program

--- src/pages/program/programPDF.11ty.js
+++ src/pages/program/programPDF.11ty.js
@@ -3,15 +3,6 @@ const fs = require("fs");
 
 const pdf = new PDFDocument();
 
-const lorem =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl. Suspendisse rhoncus nisl posuere tortor tempus et dapibus elit porta. Cras leo neque, elementum a rhoncus ut, vestibulum non nibh. Phasellus pretium justo turpis. Etiam vulputate, odio vitae tincidunt ultricies, eros odio dapibus nisi, ut tincidunt lacus arcu eu elit. Aenean velit erat, vehicula eget lacinia ut, dignissim non tellus. Aliquam nec lacus mi, sed vestibulum nunc. Suspendisse potenti. Curabitur vitae sem turpis. Vestibulum sed neque eget dolor dapibus porttitor at sit amet sem. Fusce a turpis lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;";
-
-pdf.text(lorem, {
-  columns: 2,
-  columnGap: 15,
-  height: 200,
-});
-
 class Render {
   data() {
     return {
@@ -20,9 +11,23 @@ class Render {
   }
 
   render(data) {
+    pdf.fontSize(30);
+    pdf.text("Program", { align: "center" });
+    pdf.moveDown();
+
+    data.program.forEach((day) => {
+      pdf.fontSize(20);
+      pdf.text(day.title, { columns: 2, lineGap: 2.5 });
+
+      pdf.fontSize(15);
+      day.events.forEach((event) => {
+        pdf.text(`${event.time} - ${event.title}`, { lineGap: 3.5 });
+      });
+      pdf.moveDown(1);
+    });
+
     pdf.pipe(fs.createWriteStream(data.page.outputPath));
     pdf.end();
-    return null;
   }
 }
 
